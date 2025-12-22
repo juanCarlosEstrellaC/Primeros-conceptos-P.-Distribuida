07c65dfbac350bc22f76960f99b304280ccb30bc
Juan Estrella <jcestrella@uce.edu.ec>
2025-12-22 12:56:23 -0500

    Aplicar formato

--- ejemplo03-custom-rest/build.gradle.kts
+++ ejemplo03-custom-rest/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    //id("application")
+    id("application")
     // Uso shadow para crear un jar ejecutable, con todas las dependencias, ie, autocontenido.
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
@@ -13,14 +13,12 @@ repositories {
 }
 
 dependencies {
-
-
     //CDI: Weld
     // https://mvnrepository.com/artifact/org.jboss.weld.se/weld-se-core
-   // implementation("org.jboss.weld.se:weld-se-core:6.0.2.Final")
+    // implementation("org.jboss.weld.se:weld-se-core:6.0.2.Final")
 
     //implementation("org.jboss.weld.se:weld-se-core:6.0.2.Final")
-   // https://mvnrepository.com/artifact/io.smallrye/jandex
+    // https://mvnrepository.com/artifact/io.smallrye/jandex
     //implementation("io.smallrye:jandex:3.2.7")
 
     //REST: usando RESTEasy
@@ -28,7 +26,6 @@ dependencies {
     implementation("org.jboss.resteasy:resteasy-core:6.2.12.Final")
 
     //Necesitamos un servidor
-
     implementation("org.jboss.resteasy:resteasy-undertow-cdi:6.2.12.Final")
 
 }
--- ejemplo03-custom-rest/src/main/java/com/programacion/distribuida/Ejemplo03Main.java
+++ ejemplo03-custom-rest/src/main/java/com/programacion/distribuida/Ejemplo03Main.java
@@ -7,16 +7,18 @@ import java.net.URI;
 public class Ejemplo03Main {
 
     public static void main(String[] args) throws Exception {
-
-        var configuracion = SeBootstrap.Configuration.builder().port(8080).build();
-
         // SeBootstrap es una clase que permite iniciar un servidor embebido como Undertow.
+        var configuracion = SeBootstrap.Configuration.builder()
+                .port(8080)
+                .build();
+
         SeBootstrap.start(RestApplication.class, configuracion)
                 .thenApply(instancia -> {
                     URI uri = instancia.configuration().baseUri();
                     System.out.printf("La instancia %s está corriendo en %s", instancia, uri);
                     return instancia;
-                }).exceptionally(ex -> {
+                })
+                .exceptionally(ex -> {
                     ex.printStackTrace();
                     return null;
                 });
